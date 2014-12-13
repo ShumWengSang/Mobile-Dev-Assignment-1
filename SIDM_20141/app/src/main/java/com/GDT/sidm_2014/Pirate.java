a9dc6d77bd16869555c22e2eeccbde6c35d543b9
7thValkyrie <7thAstaroth@hotmail.sg>
2014-12-14 03:45:07 +0800

    donno how use

--- SIDM_20141/app/src/main/java/com/GDT/sidm_2014/Pirate.java
+++ SIDM_20141/app/src/main/java/com/GDT/sidm_2014/Pirate.java
@@ -3,16 +3,48 @@ package com.GDT.sidm_2014;
 /**
  * Created by Shum on 10/12/2014.
  */
+import java.util.ArrayList;
+
 public class Pirate extends Entity{
+    ArrayList<Waypoint> thePoints = new ArrayList<Waypoint>();
+    Waypoint thePoint = new Waypoint();
+    Pirate(){
+
+
+
+        thePoint.currentPoint = new Vector2D(300,100);
+        thePoint.nextPoint = new Waypoint();
+        //thePoint.currentPoint = new Vector2D(1,1);
+        thePoints.add(thePoint);
+        int size = thePoints.size();
+
+    }
     void update()
     {
         EntityUpdate();
     }
+    void goLocation(Vector2D theNewPos, Entity go, float speed){
 
+        Vector2D TheDirection = Vector2D.SubtractVectors(theNewPos,go.Pos).normalized();
+
+        TheDirection.ScaleMultiplication(speed);
+        go.Vel = TheDirection;
+    }
+    boolean ReachedLocation(Vector2D thePosReached, Entity go)
+    {
+        //return 1 > (thePosReached - go->pos).Length();
+         //Vector2D.SubtractVectors(thePosReached,go.Pos).normalize();
+        if ( Vector2D.distancesquared(thePosReached, go.Pos) < 1) {
+            return (true);
+        }
+        return(false);
+    }
     void update(Vector2D PlayerPos)
     {
-        Dir = Vector2D.SubtractVectors(PlayerPos, Pos).normalized();
+        //Dir = Vector2D.SubtractVectors(PlayerPos, Pos).normalized();
+
         EntityUpdate();
-        Vel = Dir.VScaleMultiplication(10);
+        Vel = goLocation(30, this, 5);
+        //Vel = Dir.VScaleMultiplication(10);
     }
 }
---
+++ SIDM_20141/app/src/main/java/com/GDT/sidm_2014/Waypoint.java
@@ -0,0 +1,9 @@
+package com.GDT.sidm_2014;
+
+/**
+ * Created by Seventh on 13/12/2014.
+ */
+public class Waypoint {
+    Vector2D currentPoint;
+    Waypoint  nextPoint;
+}
